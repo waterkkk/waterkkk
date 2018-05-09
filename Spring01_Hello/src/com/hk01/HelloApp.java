c2108b556243f68eac3ef7bd6919041dfa4b4b4f
waterkkk <waterkyung@hanmail.net>
2018-05-09 14:26:06 +0900

    왜

--- Spring01_Hello/src/com/hk01/HelloApp.java
+++ Spring01_Hello/src/com/hk01/HelloApp.java
@@ -5,7 +5,7 @@ public class HelloApp {
 	public static void main(String[] args) {
 		MessageBean bean = new MessageBean();
 		bean.sayHello("spring");
-		System.out.println("new1");
+		System.out.println("new");
 	}
 
 }
